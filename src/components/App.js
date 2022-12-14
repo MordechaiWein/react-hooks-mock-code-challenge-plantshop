2462ed31781f865e600e3542d6d8a27bea83dcc3
Mordechai <mordwein77@gmail.com>
2022-12-14 18:33:15 -0500

    commit tree 635fd33b1fc07d15df4540b1d2b923f5619470b7

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(data => setPlants(data))
+  },[])
+
+  function addPlant(coolPlant) {
+   setPlants([...plants, coolPlant])
+  }
+  
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage addPlant={addPlant} plants={plants} search={search} setSearch={setSearch} />
     </div>
   );
 }
-
 export default App;
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, search }) {
+
+  const plantsToDisplay = plants
+  .filter((plant) => (
+    plant.name.toUpperCase().includes(search.toUpperCase())
+    ))
+    .map((plant) => {
+      return (
+      <PlantCard key={plant.id} image={plant.image} name={plant.name} price={plant.price}/>
+      )
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantsToDisplay}</ul>
   );
 }
 
